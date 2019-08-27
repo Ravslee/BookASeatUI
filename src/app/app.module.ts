2e5ed4747e1a4f409a9c51fa84fe05e9d73f5aba
ravi rajput <ravi.rr@hotmail.com>
2019-08-28 01:25:27 +0530

    functionality added!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LocalStorage } from '@ngx-pwa/local-storage';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,7 @@ import { BookingComponent } from './components/booking/booking.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorage } from '@ngx-pwa/local-storage';
+
+const KEY_SELECTED_SEATS = "selected_keys";
 
 @Component({
   selector: 'app-booking',
@@ -8,9 +11,13 @@ import { Component, OnInit } from '@angular/core';
 export class BookingComponent implements OnInit {
 
   public seats: any = {
-    rows:[],
-    cols:[]
+    rows: [],
+    cols: []
   };
+
+  public selectedSeats: any = [];
+
+
 
   public config: any = {
     sizeOfGrid: {
@@ -23,18 +30,57 @@ export class BookingComponent implements OnInit {
     ]
   }
 
-  constructor() { }
+  constructor(private localStorage: LocalStorage) { }
 
   ngOnInit() {
 
     for (let i = 1; i <= this.config.sizeOfGrid.rowSize; i++) {
       this.seats.rows.push(i);
     }
-    
+
     for (let j = 1; j <= this.config.sizeOfGrid.columnSize; j++) {
       this.seats.cols.push(j);
     }
 
+    this.localStorage.getItem(KEY_SELECTED_SEATS)
+      .toPromise()
+      .then(res => {
+        if(res){
+          this.selectedSeats = res;
+          console.log("State Loaded!");
+        }
+      });
+
   }
 
+  onSeatSelected(i, j) {
+    if (this.isSelectedSeat(i, j)) {
+      const seat = this.isSelectedSeat(i, j)
+      const index = this.selectedSeats.findIndex(el => el.column == seat.column && el.row == seat.row)
+      this.selectedSeats.splice(index, 1);
+    } else {
+      this.selectedSeats.push({ column: j, row: i })
+    }
+    console.log(this.selectedSeats);
+
+    this.localStorage.setItem(KEY_SELECTED_SEATS, this.selectedSeats)
+      .toPromise()
+      .then(res => {
+
+        console.log("State added!");
+      })
+
+  }
+
+  public isSeatUnAvailable(i, j) {
+    return this.config.booked.find(el => {
+      return el.column == j && el.row == i;
+    });
+  }
+
+  public isSelectedSeat(i, j) {
+    return this.selectedSeats.find(el => {
+      return el.column == j && el.row == i;
+    });
+  }
 }
